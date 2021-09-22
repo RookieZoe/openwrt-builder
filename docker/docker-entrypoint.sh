7e02eb9c35c4a3256b2f61d7465e42a2d68f1ad8
Rookie_Zoe <i@rookiezoe.com>
2021-09-22 18:28:11 +0800

    improve "docker-entrypoint.sh"

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -11,9 +11,9 @@ reset_reversion() {
   sed -i '/BUILD_ID=/d' /usr/lib/os-release
   sed -i '/DISTRIB_REVISION=/d' /etc/openwrt_release
   sed -i '/DISTRIB_DESCRIPTION=/d' /etc/openwrt_release
-  echo "BUILD_ID=\"$R_VERSION\"" >> /usr/lib/os-release
-  echo "DISTRIB_REVISION='$R_VERSION'" >> /etc/openwrt_release
-  echo "DISTRIB_DESCRIPTION='$R_DESCRIPTION'" >> /etc/openwrt_release
+  echo "BUILD_ID=\"$R_VERSION\"" >>/usr/lib/os-release
+  echo "DISTRIB_REVISION='$R_VERSION'" >>/etc/openwrt_release
+  echo "DISTRIB_DESCRIPTION='$R_DESCRIPTION'" >>/etc/openwrt_release
 }
 
 network_settings() {
@@ -27,41 +27,33 @@ network_settings() {
   echo "exit 0" >>/etc/uci-defaults/999-default-settings
 }
 
-data_persistence_folder() {
-  local source_dir=$(dirname $1)
-  local source_full=$(cd $1 && pwd)
-  local target_dir=$(cd $CONF_DIR && pwd)
-  local target_full=$target_dir$source_full
+data_persistence() {
+  local source_full
+  local path_type
 
-  if [ ! -h "${source_full}" ]; then
-    if [ -e "${target_full}" ]; then
-      # exists
-      echo "set link [ "$source_full" ] to folder [ "$target_full" ]"
-      rm -rf $source_full
-      mkdir -p $source_dir
-    else
-      echo "transfer folder [ "$source_full" ] to [ "$target_full" ]"
-      mkdir -p $(dirname $target_full)
-      mv $source_full $(dirname $target_full)
-    fi
-    ln -sf $target_full $source_full
+  if [ -d $1 ] && [ -w $1 ]; then
+    source_full=$(cd $1 && pwd)
+    path_type="folder"
+  elif [ -f $1 ] && [ -w $1 ]; then
+    source_full=$1
+    path_type="file"
+  # elif [ -h $1 ] && [ -w $1 ]; then
+  #   source_full=$1
+  #   path_type="link"
   fi
-}
 
-data_persistence_file() {
   local source_dir=$(dirname $1)
-  local source_full=$1
   local target_dir=$(cd $CONF_DIR && pwd)
   local target_full=$target_dir$source_full
 
-  if [ ! -h "${source_full}" ]; then
-    if [ -e "${target_full}" ]; then
+  if [ ! -h "${source_full}" ]; then # /etc/bird
+    if [ -e "${target_full}" ]; then # /data/etc/bird
       # exists
-      echo "set link [ "$source_full" ] to file [ "$target_full" ]"
+      echo "set link [ "$source_full" ] to "$path_type" [ "$target_full" ]"
       rm -rf $source_full
       mkdir -p $source_dir
     else
-      echo "transfer file [ "$source_full" ] to [ "$target_full" ]"
+      echo "transfer [ "$source_full" ] to "$path_type" [ "$target_full" ]"
       mkdir -p $(dirname $target_full)
       mv $source_full $(dirname $target_full)
     fi
@@ -78,17 +70,28 @@ if [ -n "${LAN_ADDRESS:-}" ] && [ -n "${LAN_NETMASK:-}" ] && [ -n "${LAN_GATEWAY
 fi
 
 if [ -d "${CONF_DIR}" ] && [ -w "${CONF_DIR}" ]; then
-  data_persistence_folder /root/
-  data_persistence_folder /usr/share/
+  data_persistence /root/
+
+  ls -A /usr/share/ | while read -r file; do
+    case "$file" in
+    passwall | xray)
+      data_persistence "/usr/share/"$file
+      ;;
+    *)
+      # skip
+      ;;
+    esac
+  done
 
   ls -A /etc/ | while read -r file; do
-    if [ $file != "banner" ] && [ $file != "hostname" ] && [ $file != "hosts" ] && [ $file != "openwrt_release" ] && [ $file != "openwrt_version" ] && [ $file != "resolv.conf" ]; then
-      if [ -d "/etc/${file}" ] && [ -w "/etc/${file}" ]; then
-        data_persistence_folder "/etc/"$file
-      elif [ -f "/etc/${file}" ] && [ -w "/etc/${file}" ]; then
-        data_persistence_file "/etc/"$file
-      fi
-    fi
+    case "$file" in
+    banner | hostname | hosts | openwrt_release | openwrt_version | preinit | resolv.conf)
+      # skip
+      ;;
+    *)
+      data_persistence "/etc/"$file
+      ;;
+    esac
   done
 fi
 
