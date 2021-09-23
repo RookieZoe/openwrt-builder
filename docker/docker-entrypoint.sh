ca29fdb6ddabbfef57b0584976d07237cd56f9f3
Rookie_Zoe <i@rookiezoe.com>
2021-09-23 17:48:06 +0800

    improve "docker-entrypoint.sh"

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -35,7 +35,7 @@ network_settings() {
   } >/etc/uci-defaults/999-default-settings
 }
 
-data_persistence() {
+persistence_data() {
   dp_source_full=""
   dp_path_type=""
 
@@ -45,31 +45,102 @@ data_persistence() {
   elif [ -f "$1" ] && [ -w "$1" ]; then
     dp_source_full="$1"
     dp_path_type="file"
-  # elif [ -h "$1" ] && [ -w "$1" ]; then
-  #   dp_source_full="$1"
-  #   dp_path_type="link"
+  elif [ ! -e "$1" ]; then
+    dp_source_full="$1"
+    dp_path_type="not exists file/folder"
   fi
 
   dp_source_dir=$(dirname "$1")
   dp_target_dir=$(cd "$CONF_DIR" && pwd)
   dp_target_full=$dp_target_dir$dp_source_full
 
-  if [ ! -h "${dp_source_full}" ]; then # /etc/bird
-    if [ -e "${dp_target_full}" ]; then # /data/etc/bird
-      # exists
-      echo "set link [ $dp_source_full ] to $dp_path_type [ $dp_target_full ]"
-      rm -rf "$dp_source_full"
-      mkdir -p "$dp_source_dir"
-    else
-      echo "transfer [ $dp_source_full ] to $dp_path_type [ $dp_target_full ]"
-      mkdir -p "$(dirname "$dp_target_full")"
-      mv "$dp_source_full" "$(dirname "$dp_target_full")"
-    fi
+  if [ -e "$dp_target_full" ]; then
+    echo "set link [ $dp_source_full ] to $dp_path_type [ $dp_target_full ]"
+    rm -rf "$dp_source_full"
+    mkdir -p "$dp_source_dir"
+    ln -sf "$dp_target_full" "$dp_source_full"
+  elif [ -e "$dp_source_full" ]; then
+    echo "transfer [ $dp_source_full ] to $dp_path_type [ $dp_target_full ]"
+    mkdir -p "$(dirname "$dp_target_full")"
+    mv "$dp_source_full" "$(dirname "$dp_target_full")"
     ln -sf "$dp_target_full" "$dp_source_full"
   fi
 }
 
-if [ -n "${R_VERSION}" ]; then
+persistence_paths() {
+  echo "take the 'and set' of the two lists of files..."
+  pl_target_dir=$(cd "$CONF_DIR" && pwd)
+  persistence_list="/root\n"
+  usr_share_include_names="xray.*\|passwall.*"
+  etc_exclude_names="banner.*\|hostname.*\|hosts.*\|luci.*\|modules.*\|openwrt_.*\|opkg.*\|preinit.*\|resolv.*\|uci.*\|vmware.*"
+
+  # folders in /usr/share
+  [ -e /usr/share ] && persistence_list="$persistence_list$(
+    find /usr/share -maxdepth 1 \
+      -type d -a ! -name "share" -a \
+      -regex ".*\($usr_share_include_names\)"
+  )\n"
+
+  # files in /usr/share
+  [ -e /usr/share ] && persistence_list="$persistence_list$(
+    find /usr/share -maxdepth 1 \
+      -type f -a ! -name "share" -a \
+      -regex ".*\($usr_share_include_names\)"
+  )\n"
+
+  # folders in /etc
+  [ -e /etc ] && persistence_list="$persistence_list$(
+    find /etc -maxdepth 1 \
+      -type d -a ! -name "etc" -a \
+      ! -regex ".*\($etc_exclude_names\)"
+  )\n"
+
+  # folders in /etc
+  [ -e /etc ] && persistence_list="$persistence_list$(
+    find /etc -maxdepth 1 \
+      -type f -a ! -name "etc" -a \
+      ! -regex ".*\($etc_exclude_names\)"
+  )\n"
+
+  # folders in $pl_target_dir/usr/share
+  [ -e "$pl_target_dir/usr/share" ] && persistence_list="$persistence_list$(
+    find "$pl_target_dir/usr/share" -maxdepth 1 \
+      -type d -a ! -name "$(basename "$pl_target_dir/usr/share")" -a \
+      -regex ".*\($usr_share_include_names\)" \
+      -exec sh -c 'echo ${0#$1}' {} "$pl_target_dir" \;
+  )\n"
+
+  # files in $pl_target_dir/usr/share
+  [ -e "$pl_target_dir/usr/share" ] && persistence_list="$persistence_list$(
+    find "$pl_target_dir/usr/share" -maxdepth 1 \
+      -type f -a ! -name "$(basename "$pl_target_dir/usr/share")" -a \
+      -regex ".*\($usr_share_include_names\)" \
+      -exec sh -c 'echo ${0#$1}' {} "$pl_target_dir" \;
+  )\n"
+
+  # folders in $pl_target_dir/etc
+  [ -e "$pl_target_dir/etc" ] && persistence_list="$persistence_list$(
+    find "$pl_target_dir/etc" -maxdepth 1 \
+      -type d -a ! -name "$(basename "$pl_target_dir/etc")" -a \
+      ! -regex ".*\($etc_exclude_names\)" \
+      -exec sh -c 'echo ${0#$1}' {} "$pl_target_dir" \;
+  )\n"
+
+  # files in $pl_target_dir/etc
+  [ -e "$pl_target_dir/etc" ] && persistence_list="$persistence_list$(
+    find "$pl_target_dir/etc" -maxdepth 1 \
+      -type f -a ! -name "$(basename "$pl_target_dir/etc")" -a \
+      ! -regex ".*\($etc_exclude_names\)" \
+      -exec sh -c 'echo ${0#$1}' {} "$pl_target_dir" \;
+  )\n"
+
+  # shellcheck disable=SC2059
+  printf "$persistence_list" | sort | uniq | while read -r file; do
+    [ -n "$file" ] && persistence_data "$file"
+  done
+}
+
+if [ -n "$R_VERSION" ]; then
   reset_reversion
 fi
 
@@ -77,29 +148,8 @@ if [ -n "${LAN_ADDRESS:-}" ] && [ -n "${LAN_NETMASK:-}" ] && [ -n "${LAN_GATEWAY
   network_settings
 fi
 
-if [ -d "${CONF_DIR}" ] && [ -w "${CONF_DIR}" ]; then
-  data_persistence /root/
-
-  usr_share_include_names="xray.*\|passwall.*"
-  usr_share_result="$(
-    find /usr/share -maxdepth 1 ! \
-      -type l -a \
-      -regex ".*\($usr_share_include_names\)"
-  )"
-  echo "$usr_share_result" | while read -r file; do
-    data_persistence "$file"
-  done
-
-  etc_exclude_names="banner.*\|hostname.*\|hosts.*\|openwrt_.*\|opkg.*\|preinit.*\resolv.*"
-  etc_result="$(
-    find /etc -maxdepth 1 \
-      ! -type l -a \
-      ! -name "etc" -a \
-      ! -regex ".*\($etc_exclude_names\)"
-  )"
-  echo "$etc_result" | while read -r file; do
-    data_persistence "$file"
-  done
+if [ -d "$CONF_DIR" ] && [ -w "$CONF_DIR" ]; then
+  persistence_paths
 fi
 
 exec "$@"
